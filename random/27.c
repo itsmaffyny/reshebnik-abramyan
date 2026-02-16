d2c68e75aafeaee7b45e28f8d1070bbdeef17e91
elias <ikamai044@gmail.com>
2026-02-16 23:35:32 +0200

    16.02.2026

---
+++ random/27.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Problem 3: The Hours Challenge
+// The Task: Ask the user for a large number of total seconds (e.g., 7385). 
+// Your program should calculate how many Hours, Minutes, and Seconds that represents.
+
+typedef struct{
+char name[50];
+char flower[50];
+int quant;
+}Flower; 
+
+
+int main (void){
+Flower Melissa;
+strcopy(Flower.name, "Kate");
+strcopy(Flower.flower, "melissa")
+
+printf("mins and seconds %i:%i:%i");
+
+return 0; }
