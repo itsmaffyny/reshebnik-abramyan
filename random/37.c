ce0ae7b3cb49b58b245a355a931585c5ed81bcf6
elias <ikamai044@gmail.com>
2026-02-21 20:07:06 +0200

    21.02.2026

---
+++ random/37.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+int i;
+ printf("\n");
+ for (i=0; i<6; i++){
+    printf("* ");
+ }printf("\n  ");
+ for (i=0; i<4; i++){
+    printf("* ");
+ }printf("\n   ");
+ for (i=0; i<3; i++){
+    printf("* ");
+ }printf("\n    ");
+for (i=0; i<2; i++){
+    printf("* ");
+ }
+printf("\n     ");
+printf("* ");
+return 0; 
+}
