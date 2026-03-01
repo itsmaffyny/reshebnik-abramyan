a2b00f408901d66f9cdbff2230aa33c2cd676fe5
elias <ikamai044@gmail.com>
2026-03-01 22:45:22 +0200

    01.03.2026

--- random/45.c
+++ random/45.c
@@ -8,35 +8,36 @@ void double_sum(int *storred_array, int size){
         storred_array[i] = 2*storred_array[i];
     }
 }
-void sum(int *storred_array, int size){
-    int sum=0;
-    for (int i =0; i < size; i++ ){
-        sum += storred_array[i];
-    }
-}
+// void sum(int *storred_array, int size){
+//     int sum=0;
+//     for (int i =0; i < size; i++ ){
+//         sum += storred_array[i];
+//     }
+// }
 
 int main(void){ 
+
+int k=0; int input = 0;
 int stored_array[32];
-int i=0;
-int input = 0;
-while(i != -1){
-    printf("A %d. number: ",i+1);
+
+while(k != -1 || k<32){
+    printf("A %d. number: ",k+1);
     scanf("%d", &input );
         if (input == -1){
             break;
         }
-    stored_array[i]=input;
-    i++;
+    stored_array[k]=input;
+    k++;
 }
 int size= sizeof(stored_array)/sizeof(stored_array[0]);
 printf("%d", (size));
 
-// double_sum(stored_array, size);
+double_sum(stored_array, size);
 // sum(stored_array, size);
-// printf("--------------\n");
-// for (i =0; i < size; i++ ){
-//     printf("A %d. number: %d\n", i+1, stored_array[i]);
-// }
+printf("--------------\n");
+for (int i =0; i < size; i++ ){
+    printf("A %d. number: %d\n", i+1, stored_array[i]);
+}
 
 return 0; 
 }
