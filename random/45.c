00d788f702b98f3ac59992ab52301aa8cc5920bf
elias <ikamai044@gmail.com>
2026-03-01 22:54:26 +0200

    01.03.2026

--- random/45.c
+++ random/45.c
@@ -8,6 +8,7 @@ void double_sum(int *storred_array, int size){
         storred_array[i] = 2*storred_array[i];
     }
 }
+
 // void sum(int *storred_array, int size){
 //     int sum=0;
 //     for (int i =0; i < size; i++ ){
@@ -19,7 +20,7 @@ int main(void){
 
 int k=0; int input = 0;
 int stored_array[32]={0};
-// int k;
+
 while(k != -1 || k<32){
     printf("A %d. number: ",k+1);
     scanf("%d", &input );
@@ -29,15 +30,17 @@ while(k != -1 || k<32){
     stored_array[k]=input;
     k++;
 }
+int r = k;
 int size= sizeof(stored_array)/sizeof(stored_array[0]);
-printf("%d", (size));
+// printf("%d\t", (size));
 
 double_sum(stored_array, size);
 // sum(stored_array, size);
-printf("--------------\n");
-for (int i =0; i < size; i++ ){
+printf("-----Double-----\n");
+for (int i =0; i < r; i++ ){
     printf("A %d. number: %d\n", i+1, stored_array[i]);
 }
+printf("-------Sum------\n");
 
 return 0; 
 }
