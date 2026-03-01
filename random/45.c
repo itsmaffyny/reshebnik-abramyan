10cc8e8e9eef800a5aaf859708631bc991444c07
elias <ikamai044@gmail.com>
2026-03-01 23:18:08 +0200

    01.03.2026

--- random/45.c
+++ random/45.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
-//DOUBLE 
-//SUM SUM POINT
+//DOUBLES THE GIVEN NUMBERS
+//GIVES YOU A SUM
 
 void double_sum(int *storred_array, int size){
     for (int i =0; i < size; i++ ){
         storred_array[i] = 2*storred_array[i];
     }
 }
-
-// void sum(int *storred_array, int size){
-//     int sum=0;
-//     for (int i =0; i < size; i++ ){
-//         sum += storred_array[i];
-//     }
-// }
+int sum(int *storred_array,  int r){
+    int summa =0;
+    for (int i =0; i < r; i++ ){
+        summa += storred_array[i];}
+    return summa;
+}
 
 int main(void){ 
-
 int k=0; int input = 0;
 int stored_array[32]={0};
 
@@ -30,17 +28,18 @@ while(k != -1 || k<32){
     stored_array[k]=input;
     k++;
 }
+
 int r = k;
 int size= sizeof(stored_array)/sizeof(stored_array[0]);
 // printf("%d\t", (size));
-
 double_sum(stored_array, size);
-// sum(stored_array, size);
-printf("-----Double-----\n");
+int result = sum(stored_array,  r);
+
+printf("\n-----Double-----\n");
 for (int i =0; i < r; i++ ){
     printf("A %d. number: %d\n", i+1, stored_array[i]);
 }
-printf("-------Sum------\n");
-
+printf("\n-------Sum------\n");
+printf("%d", result);
 return 0; 
 }
