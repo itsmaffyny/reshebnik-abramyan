ff07dd79b78d146e614d3a9228dda0f84c17bfcc
elias <ikamai044@gmail.com>
2026-03-02 23:55:36 +0200

    02.03.2026

---
+++ random/49.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Above main(), declare a function named add that returns an int and takes two int parameters named x and y
+// Inside main(), create an int variable named result and set it to add(5, 3)
+// Inside main(), print result using the correct format specifier
+// End main() with return 0;
+// Below main(), define the add function using the same parameter names x and y
+// Inside add, return x + y
+// The result should be 8.
+
+int add(int x, int y);
+
+int main ()
+{
+int result= add(5,3);
+printf("Result: %d", result);
+return 0;
+}
+int add(int x, int y){
+return x+y; 
+}
