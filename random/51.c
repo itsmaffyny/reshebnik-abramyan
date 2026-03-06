d9bdfeab2967c996baf0a619c0f3ce73395a9d6e
elias <ikamai044@gmail.com>
2026-03-06 23:13:31 +0200

    06.03.2026

--- random/51.c
+++ random/51.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main (void)
-{
+int main (void){
 int A[32];
-scanf("%s", A);
-printf(A);
+memset(A, 0, sizeof(int)*32);
+int B[64];
+for(int i=0; i<5; i++){
+    scanf("%d", &A[i]);
+        if (A[i]==-1){
+            exit(0);
+        }
+}
+memcpy(B, A, sizeof(int)*64);
+for(int i=0; i<32; i++){
+printf("%d",B[i]);}
 
 return 0;
 }
