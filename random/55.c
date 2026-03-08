725285b3dfb8065e6167262c8add208216004bb9
elias <ikamai044@gmail.com>
2026-03-08 22:33:48 +0200

    08.03.2026

---
+++ random/55.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main (void){
+int *pnt;
+int *pnt2;
+int *other_pnt;
+strcpy(pnt, "abc12");
+strcpy(pnt2, "xyz17");
+pnt=(char *)malloc(sizeof(char));
+pnt2=(char *)malloc(sizeof(char));
+printf("%p\n",pnt);
+printf("%p\n",pnt2);
+printf("%d\n",(int)pnt);
+printf("%d\n",(int)pnt2);
+for(int i=0; i < 100; i++){
+ printf("%c", pnt[i]);
+}
+printf("------------\n");
+pnt =(int*)realloc(pnt, 2*sizeof(int));
+pnt2 =(int*)realloc(pnt2, 2*sizeof(int));
+printf("%p\n",pnt);
+printf("%p\n",pnt2);
+printf("%d\n",(int)pnt);
+printf("%d\n",(int)pnt2);
+printf("------------\n");
+other_pnt =(int*)realloc(pnt, 20*sizeof(int));
+printf("%p\n",pnt);
+printf("%p\n",other_pnt);
+printf("%d\n",(int)pnt);
+printf("%d\n",(int)other_pnt);
+free(pnt);
+// printf("%",);
+// printf("%",);
+
+return 0;
+}
